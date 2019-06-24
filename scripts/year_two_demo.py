782be069319ad35a9db0a0241428fe5c35a11eaf
Sanic <Sanic@users.noreply.github.com>
2019-06-24 17:08:15 +0200

    added more calls to the overall high-level(

--- scripts/year_two_demo.py
+++ scripts/year_two_demo.py
@@ -18,17 +18,17 @@ if __name__ == "__main__":
 
 
     print("grasp bowl")
-    send_console_command("grasp SM_BigBowl_5 hold")
-    rospy.sleep(3)
+    send_console_command("grasp SM_BigBowl_2 hold")
+    rospy.sleep(4)
 
     # approach microwave and open
     print("Call SetRotation (Rotate towards microwave)")
-    set_rotation(0,0,-40)
+    set_rotation(0,0,-30)
     rospy.sleep(1.5)
 
     # Assumption: Starting at -234 215 106
     print("Call MoveTo (microwave)")
-    move_to((43, -25, 99))
+    move_to((43, -25.5, 99))
     rospy.sleep(3)
 
     print("Call SetRotation (Rotate towards microwave)")
@@ -37,7 +37,8 @@ if __name__ == "__main__":
 
     print("Call press open (microwave")
     send_console_command("press open")
-    rospy.sleep(3)
+    rospy.sleep(4)
+
 
     print("Call SetRotation (Rotate towards microwave)")
     set_rotation(0,0,-40)
@@ -45,13 +46,47 @@ if __name__ == "__main__":
 
     # move to mw center in order to place
     print("Call MoveTo (microwave)")
-    move_to((42, -45, 99))
+    move_to((44, -65, 99))
     rospy.sleep(3)
+    
+    # exit for now
+    # sys.exit(0) # EXIT FOR NOW
+    # exit for now
 
     print("Call place right microwave")
     send_console_command("place right microwave")
-    rospy.sleep(1.5)
+    rospy.sleep(3.5)
 
     print("Call close microwave")
     send_console_command("close microwave")
+    rospy.sleep(3.5)
+    
+    # TODO: cooking/ press start
+
+    print("Call press open (microwave")
+    send_console_command("press open")
+    rospy.sleep(3.7)
+
+    print("grasp bowl again")
+    send_console_command("grasp SM_BigBowl_2 hold")
+    rospy.sleep(3.5)
+
+    print("Call close microwave")
+    send_console_command("close microwave")
+    rospy.sleep(3.5)
+
+    print("Call SetRotation (Rotate roughly towards table )")
+    set_rotation(0,0,160)
     rospy.sleep(1.5)
+
+    print("Call MoveTo (table)")
+    move_to((-391, -12, 99))
+    rospy.sleep(4)
+
+    print("Call SetRotation (Rotate towards table for placemen)")
+    set_rotation(0,0,240)
+    rospy.sleep(1.7)
+
+    print("Place right table")
+    send_console_command("place right table")
+    rospy.sleep(2.5)
