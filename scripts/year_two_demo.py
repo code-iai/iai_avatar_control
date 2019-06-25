d4d5f18cb07c4ad75b7f3b4484fb29a8d86285a5
Sanic <Sanic@users.noreply.github.com>
2019-06-25 16:09:34 +0200

    latest adjustments

--- scripts/year_two_demo.py
+++ scripts/year_two_demo.py
@@ -10,44 +10,46 @@ if __name__ == "__main__":
     print("Starting program")
     rospy.sleep(2)
 
-    # Assumption: Starting at -234 215 106
+    # Assumption: Starting at -36 237 106
     print("Call MoveTo (bowl)")
-    move_to((-112, -12, 99))
-    rospy.sleep(3)
+    move_to((-36, -78,106))
+    rospy.sleep(3.5)
 
+
+    print("Call SetRotation (Rotate towards bowl)")
+    set_rotation(0,0,-180)
+    rospy.sleep(1.5)
 
 
     print("grasp bowl")
     send_console_command("grasp SM_BigBowl_2 hold")
-    rospy.sleep(4)
+    rospy.sleep(3)
 
     # approach microwave and open
     print("Call SetRotation (Rotate towards microwave)")
-    set_rotation(0,0,-30)
+    set_rotation(0,0,-20)
     rospy.sleep(1.5)
 
-    # Assumption: Starting at -234 215 106
     print("Call MoveTo (microwave)")
     move_to((43, -25.5, 99))
-    rospy.sleep(3)
+    rospy.sleep(2.5)
 
     print("Call SetRotation (Rotate towards microwave)")
     set_rotation(0,0,-20)
-    rospy.sleep(1.5)
+    rospy.sleep(1.3)
 
     print("Call press open (microwave")
     send_console_command("press open")
     rospy.sleep(4)
 
-
-    print("Call SetRotation (Rotate towards microwave)")
-    set_rotation(0,0,-40)
-    rospy.sleep(1.5)
+    print("Call SetRotation (Rotate towards microwave TO PLACE)")
+    set_rotation(0,0,-35)
+    rospy.sleep(1.3)
 
     # move to mw center in order to place
-    print("Call MoveTo (microwave)")
-    move_to((44, -65, 99))
-    rospy.sleep(3)
+    print("Call MoveTo (microwave, to place)")
+    move_to((44, -60, 99))
+    rospy.sleep(1.5)
     
     # exit for now
     # sys.exit(0) # EXIT FOR NOW
@@ -61,7 +63,9 @@ if __name__ == "__main__":
     send_console_command("close microwave")
     rospy.sleep(3.5)
     
-    # TODO: cooking/ press start
+    print("Call press start (microwave")
+    send_console_command("press start")
+    rospy.sleep(3.7)
 
     print("Call press open (microwave")
     send_console_command("press open")
@@ -80,13 +84,44 @@ if __name__ == "__main__":
     rospy.sleep(1.5)
 
     print("Call MoveTo (table)")
-    move_to((-391, -12, 99))
-    rospy.sleep(4)
+    move_to((-402, -15, 99))
+    rospy.sleep(4.5)
 
     print("Call SetRotation (Rotate towards table for placemen)")
     set_rotation(0,0,240)
     rospy.sleep(1.7)
 
+
+    # assumption: grasped bowl should be at -408 -72 88
     print("Place right table")
     send_console_command("place right table")
-    rospy.sleep(2.5)
+    rospy.sleep(3)
+
+    print("Call SetRotation (Rotate away from table)")
+    set_rotation(0,0,180)
+    rospy.sleep(1.7)
+
+    print("Call MoveTo (away)")
+    move_to((-630, 0, 99))
+    rospy.sleep(2)
+
+    print("Call SetRotation")
+    set_rotation(0,0,270)
+    rospy.sleep(1.7)
+
+    print("Call MoveTo (table on the other side)")
+    move_to((-630, -254, 99))
+    rospy.sleep(2)
+
+    print("Call SetRotation")
+    set_rotation(0,0,0)
+    rospy.sleep(1.7)
+
+    print("Call MoveTo (move towards table again)")
+    move_to((-420, -254, 99))
+    rospy.sleep(2)
+
+    print("Call SetRotation")
+    set_rotation(0,0,90)
+    rospy.sleep(1.7)
+
